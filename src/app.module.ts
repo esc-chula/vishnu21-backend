429c9f46e0aedb4c0918e19e609d2e08dc745d1b
Nasmeen Islam <55419362+NasmeenI@users.noreply.github.com>
2023-06-22 00:22:03 +0700

    Revert "GET and DELETE score history"

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,9 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { GroupController } from './group/group.controller';
 import { GroupModule } from './group/group.module';
-import { ScoringModule } from './scoring/scoring.module';
 
 @Module({
-  imports: [PrismaModule, GroupModule, ScoringModule,],
+  imports: [PrismaModule, GroupModule],
   controllers: [AppController],
   providers: [AppService],
 })

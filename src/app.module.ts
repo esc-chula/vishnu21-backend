44259497813466244988164dc0afa7028ca81d56
hiishadow <puripat.khomnoy@gmail.com>
2023-06-22 01:19:41 +0700

    feat: group info

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { GroupController } from './group/group.controller';
-import { GroupModule } from './group/group.module';
+import { GroupsModule } from './groups/groups.module';
 
 @Module({
-  imports: [PrismaModule, GroupModule],
+  imports: [PrismaModule, GroupsModule],
   controllers: [AppController],
   providers: [AppService],
 })

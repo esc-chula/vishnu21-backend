c83393408536acbcb9c1ae9bdd2b29d4abfbc31b
boomchanotai <boom9387@hotmail.com>
2023-06-21 14:00:12 +0700

    fix: remove hard code hahaha TT

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private prisma: PrismaService) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
-  async getHello(): Promise<any> {
-    return await this.prisma.group.create({
-      data: {
-        houseName: 'Boom',
-        group: 'M',
-      },
-    });
+  getHello(): string {
+    return this.appService.getHello();
   }
 }

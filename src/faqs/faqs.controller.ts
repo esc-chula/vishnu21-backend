a70fc7047e5812aa47479a0e59195eda4725bd3b
khunfloat <60844731+khunfloat@users.noreply.github.com>
2023-07-05 02:15:24 +0700

    fix: something nidnoi kub 😂 x2 correct the variable and its type

--- src/faqs/faqs.controller.ts
+++ src/faqs/faqs.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { FaqsService } from './faqs.service';
 
 @Controller('faq')
 export class FaqsController {
-  constructor(private FaqsService: FaqsService) {}
+  constructor(private faqsService: FaqsService) {}
 
   @Get('/:event')
   async getFaqByEvent(@Param('event') event: string) {
-    return await this.FaqsService.getFaqByEvent(event);
+    return await this.faqsService.getFaqByEvent(event);
   }
 }
